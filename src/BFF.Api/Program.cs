e08aa4013d339b5cf7cdd11d3be2aaa555bac1a5
ZaibiMed <mohammed.zaibi@outlook.com>
2024-11-30 23:29:15 +0100

    Update Program.cs with middleware and service configuration

---
+++ src/BFF.Api/Program.cs
@@ -0,0 +1,30 @@
+using BFF.Api.Extensions;
+using BFF.Api.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure gRPC and application services
+builder.Services.AddGrpcServices(builder.Configuration);
+builder.Services.AddApplicationServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
